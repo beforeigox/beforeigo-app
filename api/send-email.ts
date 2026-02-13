dae9dc6087710147a033becff348ee27ed0b4e56
Kasey Lycar <kaseylycar@Kaseys-MBP.home.local>
2026-02-13 01:52:23 -0800

    Fix CORS to allow all origins

--- api/send-email.ts
+++ api/send-email.ts
@@ -95,7 +95,7 @@ const emailTemplates = {
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Add CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://beforeigo.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
